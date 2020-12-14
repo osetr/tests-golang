14fde07867bc2930d1a5984b57a82f85f2fa37e8
anton <anton.osetrov@hard-soft.dev>
2020-12-15 00:23:03 +0200

    done with tests

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -17,16 +17,15 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/io/
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
-
+		out.ReadFrom(in)
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
 	{
 		in := new(bytes.Buffer)
 		in.WriteString("hello world")
-
 		out := new(bytes.Buffer)
-
+		out.Write(in.Bytes()[:5])
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
